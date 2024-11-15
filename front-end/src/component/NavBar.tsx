1f0384980e810977252c8ab59526466560f2947e
Sanskar Suri <sanskarsuri50@gmail.com>
2024-11-15 18:47:55 +0530

    Added react components

--- front-end/src/component/NavBar.tsx
+++ front-end/src/component/NavBar.tsx
@@ -1,65 +1,90 @@
+// Importing required CSS and Bootstrap styling for the NavBar component
 import "../Style/NavBar.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
 import handleSearch from "../pages/Home";
-function NavBar() {
-  const [activeLink, setActiveLink] = useState("home");
-  
-    const handleLinkClick = (name: string) => {
-      setActiveLink(name);
-    };
-    return (
-      <header className="header-area header-sticky">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <nav className="main-nav">
-                <a href="/" className="logo">
-                  <h4>
-                    <em>Gaming</em> Strore
-                  </h4>
-                </a>
-                <div className="search-input">
-                  {/* <form id="search" onSubmit={handleSearch}>
-                    <input
-                      type="text"
-                      placeholder="Type Something"
-                      id="searchText"
-                      name="searchKeyword"
-                    />
-                    <i className="fa fa-search"></i>
-                  </form> */}
-                </div>
-                <ul className="nav">
-                  <li>
-                    <a href="/" className="active">
-                      Home
-                    </a>
-                  </li>
 
-                  {/* <li>
-                    <a href="/checkout">checkout</a>
-                  </li> */}
-                  <li>
-                    <a href="/cart">Cart</a>
-                  </li>
-                  <li>
-                    <a href="/login">login</a>
-                  </li>
-                  <li>
-                    <a href="/profile">
-                      Profile <img src="src\assets\profile-header.jpg" alt="" />
-                    </a>
-                  </li>
-                </ul>
-                <a className="menu-trigger">
-                  <span>Menu</span>
-                </a>
-              </nav>
-            </div>
+// Define the NavBar functional component
+function NavBar() {
+  // useState hook to manage the state of the active link in the navigation bar
+  const [activeLink, setActiveLink] = useState("home");
+
+  // Function to handle link click events and update the active link state
+  const handleLinkClick = (name: string) => {
+    setActiveLink(name);
+  };
+
+  // Render the navigation bar
+  return (
+    <header className="header-area header-sticky">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <nav className="main-nav">
+              {/* Logo section with a clickable link to the homepage */}
+              <a href="/" className="logo">
+                <h4>
+                  <em>Gaming</em> Store
+                </h4>
+              </a>
+
+              {/* Search input section (currently commented out) */}
+              <div className="search-input">
+                {/* 
+                <form id="search" onSubmit={handleSearch}>
+                  <input
+                    type="text"
+                    placeholder="Type Something"
+                    id="searchText"
+                    name="searchKeyword"
+                  />
+                  <i className="fa fa-search"></i>
+                </form> 
+                */}
+              </div>
+
+              {/* Navigation links */}
+              <ul className="nav">
+                <li>
+                  {/* Home link with an "active" class */}
+                  <a href="/" className="active" onClick={() => handleLinkClick("home")}>
+                    Home
+                  </a>
+                </li>
+
+                {/* Cart link */}
+                <li>
+                  <a href="/cart" onClick={() => handleLinkClick("cart")}>
+                    Cart
+                  </a>
+                </li>
+
+                {/* Login link */}
+                <li>
+                  <a href="/login" onClick={() => handleLinkClick("login")}>
+                    Login
+                  </a>
+                </li>
+
+                {/* Profile link with a placeholder profile image */}
+                <li>
+                  <a href="/profile" onClick={() => handleLinkClick("profile")}>
+                    Profile <img src="src/assets/profile-header.jpg" alt="Profile" />
+                  </a>
+                </li>
+              </ul>
+
+              {/* Menu trigger for smaller screens (mobile view) */}
+              <a className="menu-trigger">
+                <span>Menu</span>
+              </a>
+            </nav>
           </div>
         </div>
-      </header>
-    );
-  }
+      </div>
+    </header>
+  );
+}
+
+// Exporting the NavBar component for use in other parts of the application
 export default NavBar;
--- front-end/src/component/card.tsx
+++ front-end/src/component/card.tsx
@@ -1,18 +1,28 @@
+// Importing required CSS styling for the Card component
 import "../Style/home.css";
 
+// Defining the Props interface to specify the types of props expected by the Card component
 interface Props {
-  name: string;
-  price: number;
-  imgsrc: string;
-  category: string;
+  name: string;       // Name of the item
+  price: number;      // Price of the item
+  imgsrc: string;     // Image source URL for the item
+  category: string;   // Category of the item
 }
 
+// Defining the Card functional component with destructured props
 const Card = ({ name, price, imgsrc, category }: Props) => {
   return (
     <div className="item">
-      <img src={imgsrc} alt="" />
+      {/* Displaying the item image */}
+      <img src={imgsrc} alt={`${name}`} />
+
+      {/* Displaying the item name */}
       <h4>{name}</h4>
+
+      {/* Displaying the item category */}
       <p>{category}</p>
+
+      {/* Displaying the item price */}
       <ul>
         <li>
           <i className="fa fa-dollar-sign">{price}</i>
@@ -22,4 +32,5 @@ const Card = ({ name, price, imgsrc, category }: Props) => {
   );
 };
 
+// Exporting the Card component for use in other parts of the application
 export default Card;
