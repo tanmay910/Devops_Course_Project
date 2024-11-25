5b50ddc09845484054bb591471d682779590c8ca
Ayush Jain <96832194+Ayushjain0@users.noreply.github.com>
2024-11-25 14:52:17 +0530

    updates server.js

--- User/server.js
+++ User/server.js
@@ -6,25 +6,25 @@ const app = express(); // Initialize an Express application
 // Load environment variables from the .env file
 require('dotenv').config();
 
-// Import database connection configuration
+// Import database connection configuration.
 require('./config/db_conn');
 
-// Define the server port, defaulting to 9001 if not provided in environment variables
+// Define the server port, defaulting to 9001 if not provided in environment variables.
 const port = process.env.PORT || 9001;
 
 // Middleware to enable CORS (allow cross-origin requests)
 app.use(cors());
 
-// Middleware to parse JSON payloads in incoming requests
+// Middleware to parse JSON payloads in incoming requests.
 app.use(express.json());
 
-// Middleware to parse URL-encoded form data
+// Middleware to parse URL-encoded form data.
 app.use(express.urlencoded({ extended: true }));
 
-// Route for handling user-related requests
+// Route for handling user-related requests.
 app.use("/users", require("./routes/userRouter"));
 
-// Start the server and listen on the specified port
+// Start the server and listen on the specified port.
 app.listen(port, () => {
     console.log(`Server running on port ${port}`); // Log a message when the server is up and running
 });
